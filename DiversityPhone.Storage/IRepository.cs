d144271ec6cd98cf74ae7c52254b1bae63f9cfee
Georg Rollinger <rthundr@live.de>
2013-11-29 13:46:07 -0300

    Generic Repository Impl References #60

---
+++ DiversityPhone.Storage/IRepository.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Linq.Expressions;
+namespace DiversityPhone.Storage
+{
+    public interface IEntity<T>
+    {
+        Expression<Func<T, bool>> IDEqualsExpression();
+    }
+
+    public interface IRepository
+    {
+        void Add<T>(T entity) where T : class, IEntity<T>;
+        IEnumerable<T> GetAll<T>() where T : class, IEntity<T>;
+        IEnumerable<T> Get<T>(Expression<Func<T, bool>> filter) where T : class, IEntity<T>;
+        T Single<T>(Expression<Func<T, bool>> filter) where T : class, IEntity<T>;
+        void Update<T>(T entity) where T : class, IEntity<T>;
+        void Delete<T>(T entity) where T : class, IEntity<T>;
+    }
+
+    public class FieldDataRepository : IRepository
+    {
+        private readonly Func<DataContext> GetContext;
+
+        public FieldDataRepository(Func<DataContext> GetContext)
+        {
+            this.GetContext = GetContext;
+
+            CreateDBIfNecessary();
+        }
+
+        private void CreateDBIfNecessary()
+        {
+            using (var ctx = GetContext())
+            {
+                if (!ctx.DatabaseExists())
+                {
+                    ctx.CreateDatabase();
+                }
+            }
+        }
+
+        public void ClearDatabase()
+        {
+            using (var ctx = GetContext())
+            {
+                if (ctx.DatabaseExists())
+                {
+                    ctx.DeleteDatabase();
+                }
+                ctx.CreateDatabase();
+            }
+        }
+
+        public void Add<T>(T entity) where T : class, IEntity<T>
+        {
+            using (var ctx = GetContext())
+            {
+                var table = ctx.GetTable<T>();
+
+                table.InsertOnSubmit(entity);
+
+                ctx.SubmitChanges();
+            }
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : class, IEntity<T>
+        {
+            return QueryImpl<T>(x => true);
+        }
+
+        public IEnumerable<T> Get<T>(Expression<Func<T, bool>> filter) where T : class, IEntity<T>
+        {
+            return QueryImpl(filter);
+        }
+
+        public T Single<T>(Expression<Func<T, bool>> filter) where T : class, IEntity<T>
+        {
+            return QueryImpl(filter).FirstOrDefault();
+        }
+
+        private IEnumerable<T> QueryImpl<T>(Expression<Func<T, bool>> filter) where T : class, IEntity<T>
+        {
+            using (var ctx = GetContext())
+            {
+                var q = ctx.GetTable<T>()
+                           .Where(filter);
+
+                foreach (var x in q)
+                {
+                    yield return x;
+                }
+            }
+        }
+
+        public void Update<T>(T entity) where T : class, IEntity<T>
+        {
+            var unmodifiedEntity = Single(entity.IDEqualsExpression());
+
+            if (unmodifiedEntity != null)
+            {
+                using (var ctx = GetContext())
+                {
+                    var table = ctx.GetTable<T>();
+
+                    table.Attach(entity, unmodifiedEntity);
+
+                    ctx.SubmitChanges();
+                }
+            }
+        }
+
+        public void Delete<T>(T entity) where T : class, IEntity<T>
+        {
+            using (var ctx = GetContext())
+            {
+                var table = ctx.GetTable<T>();
+                var attachedEntity = table.Where(entity.IDEqualsExpression()).SingleOrDefault();
+                if (attachedEntity != null)
+                {
+                    table.DeleteOnSubmit(attachedEntity);
+                }
+
+                ctx.SubmitChanges();
+            }
+        }
+
+
+
+    }
+}
---
+++ DiversityPhone.UnitTest/RepositoryTest.cs
@@ -0,0 +1,124 @@
+﻿using DiversityPhone.Storage;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Linq;
+
+namespace DiversityPhone.UnitTest
+{
+    [Table]
+    internal class TestEntity : IEntity<TestEntity>
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int? ID { get; set; }
+
+        [Column]
+        public string Name { get; set; }
+
+        [Column]
+        public int Search { get; set; }
+
+        [Column]
+        public DateTime Timestamp { get; set; }
+
+        [Column]
+        public double Altitude { get; set; }
+
+        public System.Linq.Expressions.Expression<System.Func<TestEntity, bool>> IDEqualsExpression()
+        {
+            return x => x.ID == ID;
+        }
+
+        public TestEntity()
+        {
+            // ATTENTION: Default can't be stored in DB
+            Timestamp = DateTime.Now;
+        }
+    }
+
+    internal class FieldDataContext : DataContext
+    {
+        public FieldDataContext()
+            : base("isostore:/test.sdf")
+        {
+
+        }
+#pragma warning disable 0649
+        public Table<TestEntity> Entities;
+#pragma warning restore 0649
+    }
+
+    [TestClass]
+    public class RepositoryTest
+    {
+        private FieldDataRepository Target;
+
+        public RepositoryTest()
+        {
+            Target = new FieldDataRepository(() => new FieldDataContext());
+            Target.ClearDatabase();
+        }
+
+        [TestMethod]
+        public void InsertSetsID()
+        {
+            var ent = new TestEntity() { ID = null, Name = "Test" };
+
+            Target.Add(ent);
+
+            Assert.AreNotEqual(null, ent.ID);
+        }
+
+        [TestMethod]
+        public void GetGetsAll()
+        {
+            var entities = new[] {
+                new TestEntity() { Search = 1000 },
+                new TestEntity() { Search = 1000 },
+                new TestEntity() { Search = 1000 }
+            };
+
+            foreach (var ent in entities)
+            {
+                Target.Add(ent);
+            }
+            var exp = entities.Select(e => e.ID).ToList();
+            var res = Target.Get<TestEntity>(e => e.Search == 1000).Select(x => x.ID).ToList();
+
+            CollectionAssert.AreEquivalent(exp, res);
+        }
+
+
+        [TestMethod]
+        public void DeleteDeletes()
+        {
+            var ent = new TestEntity();
+
+            Target.Add(ent);
+
+            Target.Delete(ent);
+
+            var res = Target.Get<TestEntity>(x => x.ID == ent.ID);
+
+            Assert.IsTrue(res.Count() == 0);
+        }
+
+        [TestMethod]
+        public void UpdateUpdates()
+        {
+            var ent = new TestEntity() { Altitude = 1.0, Name = "Update", Search = 100, Timestamp = DateTime.Now };
+            var exp = new TestEntity() { Search = 1000 };
+
+            Target.Add(ent);
+
+            exp.ID = ent.ID;
+
+            Target.Update(exp);
+
+            var inStore = Target.Single<TestEntity>(exp.IDEqualsExpression());
+
+            Assert.AreEqual(exp.Search, inStore.Search);
+        }
+    }
+}
